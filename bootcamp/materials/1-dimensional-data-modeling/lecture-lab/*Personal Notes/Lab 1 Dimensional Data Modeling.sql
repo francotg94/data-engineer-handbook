b5af09f9f309c217b71bb47a9fe43f2b69075209
francotg94 <118710729+francotg94@users.noreply.github.com>
2024-11-19 15:44:40 -0800

    adding lab notes for lab 1 and lab 2

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/*Personal Notes/Lab 1 Dimensional Data Modeling.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/*Personal Notes/Lab 1 Dimensional Data Modeling.sql
@@ -1,12 +1,10 @@
--- SELECT * FROM player_seasons;
 -- CREATE TYPE season_stats AS (
 --                         season INTEGER,
 --                         gp INTEGER,
 --                         pts REAL,
 --                         reb REAL,
 --                         ast REAL
---                             )
-
+--                          )
 -- CREATE TYPE scoring_class AS ENUM ('star', 'good', 'average', 'bad');
 --
 -- CREATE TABLE players (
@@ -21,98 +19,61 @@
 --     scoring_class scoring_class,
 --     years_since_last_season INTEGER,
 --     current_season INTEGER,
---     PRIMARY KEY(player_name, current_season)
+--     is_active BOOLEAN,
+--     PRIMARY KEY (player_name, current_season)
 -- );
 
---Query run earlier in lab
---SELECT MIN(season) FROM player_seasons;
 
---Query run about half way through lab
--- INSERT INTO players
-
--- DROP TABLE players;
---
-INSERT INTO players (
-    player_name,
-    height,
-    college,
-    country,
-    draft_year,
-    draft_round,
-    draft_number,
-    season_stats,
-    scoring_class,
-    years_since_last_season,
-    current_season
-)
-WITH yesterday AS (
+INSERT INTO players
+WITH yesterday AS(
     SELECT * FROM players
-    WHERE current_season = 1996
+    WHERE current_season = 2000
 ),
-today AS (
-    SELECT * FROM player_seasons
-    WHERE season = 1997
-)
+    today AS (
+        SELECT * FROM player_seasons
+        WHERE season = 2001
+    )
+
 SELECT
-    COALESCE(t.player_name, y.player_name) AS player_name,
-    COALESCE(t.height, y.height) AS height,
-    COALESCE(t.college, y.college) AS college,
-    COALESCE(t.country, y.country) AS country,
-    COALESCE(t.draft_year, y.draft_year) AS draft_year,
-    COALESCE(t.draft_round, y.draft_round) AS draft_round,
-    COALESCE(t.draft_number, y.draft_number) AS draft_number,
-    CASE
-        WHEN y.season_stats IS NULL THEN ARRAY[
-            ROW(
+        COALESCE(t.player_name, y.player_name) AS player_name,
+        COALESCE(t.height, y.height) AS height,
+        COALESCE(t.college, y.college) AS college,
+        COALESCE(t.country, y.country) AS country,
+        COALESCE(t.draft_year, y.draft_year) AS draft_year,
+        COALESCE(t.draft_round, y.draft_round) AS draft_round,
+        COALESCE(t.draft_number, y.draft_number) AS draft_number,
+        CASE WHEN y.season_stats IS NULL
+            THEN ARRAY[ROW(
                 t.season,
                 t.gp,
                 t.pts,
                 t.reb,
                 t.ast
-            )::season_stats
-        ]
-        WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY[
-            ROW(
+                )::season_stats]
+        WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY[ROW(
                 t.season,
                 t.gp,
                 t.pts,
                 t.reb,
                 t.ast
-            )::season_stats
-        ]
-        ELSE y.season_stats
-    END AS season_stats,
-    CASE
-        WHEN t.season IS NOT NULL THEN
-            CASE
-                WHEN t.pts > 20 THEN 'star'
+                ):: season_stats]
+      ELSE y.season_stats
+      END as season_stats,
+      CASE
+          WHEN t.season IS NOT NULL THEN
+            CASE WHEN t.pts > 20 THEN 'star'
                 WHEN t.pts > 15 THEN 'good'
                 WHEN t.pts > 10 THEN 'average'
                 ELSE 'bad'
-            END::scoring_class
-        ELSE y.scoring_class
-    END AS scoring_class,
-    CASE
-        WHEN t.season IS NOT NULL THEN 0
+           END::scoring_class
+           ELSE y.scoring_class
+      END as scoring_class,
+
+    CASE WHEN t.season IS NOT NULL THEN 0
         ELSE y.years_since_last_season + 1
-    END AS years_since_last_season,
-    COALESCE(t.season, y.current_season + 1) AS current_season
-FROM today t
-FULL OUTER JOIN yesterday y
-    ON t.player_name = y.player_name
-ON CONFLICT (player_name, current_season) DO NOTHING;
+            END as years_since_last_season,
 
-SELECT * FROM players
-WHERE current_season = 1997;
--- AND player_name = 'Michael Jordan';
+      COALESCE(t.season, y.current_season + 1) as current_season
+FROM today t FULL OUTER JOIN yesterday y
+    ON t.player_name = y.player_name;
 
--- WITH unnested AS (
--- SELECT player_name,
---        UNNEST(season_stats) :: season_stats AS season_stats
---         FROM players
--- WHERE current_season = 2001
--- AND player_name = 'Michael Jordan'
--- )
--- SELECT player_name,
---        (season_stats::season_stats).*
--- FROM unnested
---
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/*Personal Notes/Lab 2 Dimensional Data Modeling.sql
@@ -0,0 +1,138 @@
+--
+-- CREATE TABLE players (
+--     player_name TEXT,
+--     height TEXT,
+--     college TEXT,
+--     country TEXT,
+--     draft_year TEXT,
+--     draft_round TEXT,
+--     draft_number TEXT,
+--     season_stats season_stats[],
+--     scoring_class scoring_class,
+--     years_since_last_season INTEGER,
+--     current_season INTEGER,
+--     is_active BOOLEAN,
+--     PRIMARY KEY (player_name, current_season)
+-- );
+
+-- CREATE TABLE players_scd(
+--     player_name TEXT,
+--     scoring_class scoring_class,
+--     is_active BOOLEAN,
+--     start_season INTEGER,
+--     end_season INTEGER,
+--     current_season INTEGER,
+--     PRIMARY KEY (player_name, start_season)
+-- );
+
+
+-- INSERT INTO players
+-- WITH years AS (
+--     SELECT *
+--     FROM GENERATE_SERIES(1996, 2022) AS season
+-- ), p AS (
+--     SELECT
+--         player_name,
+--         MIN(season) AS first_season
+--     FROM player_seasons
+--     GROUP BY player_name
+-- ), players_and_seasons AS (
+--     SELECT *
+--     FROM p
+--     JOIN years y
+--         ON p.first_season <= y.season
+-- ), windowed AS (
+--     SELECT
+--         pas.player_name,
+--         pas.season,
+--         ARRAY_REMOVE(
+--             ARRAY_AGG(
+--                 CASE
+--                     WHEN ps.season IS NOT NULL
+--                         THEN ROW(
+--                             ps.season,
+--                             ps.gp,
+--                             ps.pts,
+--                             ps.reb,
+--                             ps.ast
+--                         )::season_stats
+--                 END)
+--             OVER (PARTITION BY pas.player_name ORDER BY COALESCE(pas.season, ps.season)),
+--             NULL
+--         ) AS seasons
+--     FROM players_and_seasons pas
+--     LEFT JOIN player_seasons ps
+--         ON pas.player_name = ps.player_name
+--         AND pas.season = ps.season
+--     ORDER BY pas.player_name, pas.season
+-- ), static AS (
+--     SELECT
+--         player_name,
+--         MAX(height) AS height,
+--         MAX(college) AS college,
+--         MAX(country) AS country,
+--         MAX(draft_year) AS draft_year,
+--         MAX(draft_round) AS draft_round,
+--         MAX(draft_number) AS draft_number
+--     FROM player_seasons
+--     GROUP BY player_name
+-- )
+-- SELECT
+--     w.player_name,
+--     s.height,
+--     s.college,
+--     s.country,
+--     s.draft_year,
+--     s.draft_round,
+--     s.draft_number,
+--     seasons AS season_stats,
+--     CASE
+--         WHEN (seasons[CARDINALITY(seasons)]::season_stats).pts > 20 THEN 'star'
+--         WHEN (seasons[CARDINALITY(seasons)]::season_stats).pts > 15 THEN 'good'
+--         WHEN (seasons[CARDINALITY(seasons)]::season_stats).pts > 10 THEN 'average'
+--         ELSE 'bad'
+--     END::scoring_class AS scoring_class,
+--     w.season - (seasons[CARDINALITY(seasons)]::season_stats).season as years_since_last_active,
+--     w.season,
+--     (seasons[CARDINALITY(seasons)]::season_stats).season = season AS is_active
+-- FROM windowed w
+-- JOIN static s
+--     ON w.player_name = s.player_name;
+
+INSERT INTO players_scd
+WITH with_previous AS (SELECT player_name,
+                              current_season,
+                              scoring_class,
+                              is_active,
+                              LAG(scoring_class, 1) OVER (PARTITION BY player_name ORDER BY current_season)as previous_scoring_class,
+                              LAG(is_active, 1) OVER (PARTITION BY player_name ORDER BY current_season) as previous_is_active
+                       FROM players
+                       WHERE current_season <= 2021
+),
+        with_indicators AS (
+    SELECT *,
+            CASE
+                WHEN scoring_class <> previous_scoring_class THEN 1
+                WHEN is_active <> previous_is_active THEN 1
+                ELSE 0
+            END AS change_indicator
+    FROM with_previous
+
+        ),
+        with_streaks AS (
+            SELECT *,
+         SUM(change_indicator)
+            OVER (PARTITION BY player_name ORDER BY current_season) AS streak_identifier
+    FROM with_indicators
+        )
+
+        SELECT player_name,
+               scoring_class,
+               is_active,
+               MIN(current_season) as start_season,
+               MAX(current_season) as end_season,
+               2021 AS current_season
+            FROM with_streaks
+        GROUP BY player_name, streak_identifier, is_active, scoring_class
+        ORDER BY player_name, streak_identifier
+
