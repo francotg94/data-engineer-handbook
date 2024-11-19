7bb971260b5945db9faca6a7913b2d307e694d9f
francotg94 <118710729+francotg94@users.noreply.github.com>
2024-11-19 15:45:33 -0800

    updating local fork

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/*Personal Notes/Lab 1 Dimensional Data Modeling.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/*Personal Notes/Lab 1 Dimensional Data Modeling.sql
@@ -1,29 +1,3 @@
--- CREATE TYPE season_stats AS (
---                         season INTEGER,
---                         gp INTEGER,
---                         pts REAL,
---                         reb REAL,
---                         ast REAL
---                          )
--- CREATE TYPE scoring_class AS ENUM ('star', 'good', 'average', 'bad');
---
--- CREATE TABLE players (
---     player_name TEXT,
---     height TEXT,
---     college TEXT,
---     country TEXT,
---     draft_year TEXT,
---     draft_round TEXT,
---     draft_number TEXT,
---     season_stats season_stats[],
---     scoring_class scoring_class,
---     years_since_last_season INTEGER,
---     current_season INTEGER,
---     is_active BOOLEAN,
---     PRIMARY KEY (player_name, current_season)
--- );
-
-
 INSERT INTO players
 WITH yesterday AS(
     SELECT * FROM players
@@ -76,4 +50,3 @@ SELECT
       COALESCE(t.season, y.current_season + 1) as current_season
 FROM today t FULL OUTER JOIN yesterday y
     ON t.player_name = y.player_name;
-
